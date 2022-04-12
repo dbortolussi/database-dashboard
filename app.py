cb7d3f5f513377bd6aad67226bb63d6d7622e79e
danielbortolussi <102590946+danielbortolussi@users.noreply.github.com>
2022-04-12 08:13:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from dash import Dash, html, dcc
 
+app = Dash(__name__)
+
+app.layout = html.Div("Dashboard", className="panel-container", children=[
+  html.Div("Panel 1", className="panel"),
+  html.Div("Panel 2", className="panel"),
+  html.Div("Panel 3", className="panel"),
+  html.Div("Panel 4", className="panel"),
+  html.Div("Panel 5", className="panel"),
+  html.Div("Panel 6", className="panel"),
+]  
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
